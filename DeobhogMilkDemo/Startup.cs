cd8e16d40dafbf8a5e5a823bbf24ca35ee616d75
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2020-06-21 20:45:59 +0530

    Add project files.

---
+++ DeobhogMilkDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeobhogMilkDemo.Startup))]
+namespace DeobhogMilkDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
